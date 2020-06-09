63bad015ccc53020a7aa6001ba2f5d5a8de05700
volodymyr-masalitin <66335450+volodymyr-masalitin@users.noreply.github.com>
2020-06-09 18:31:14 +0200

    Add files via upload
    The ready pack of files/scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# netcomm
+This repository consists of the following files
+
+ 1. utils.py containing some useful functions
+ 2. experiment_script_original.py containing the original experiment script (and template) without usage of multiprocessing
+ 3. experiment_script_multiprocessing containing the edited version of the experiment script using multiprocessing features
+ 4. visualisation_script.py containing a template of plotting procedures
+ 5. compare_time.py containing the setup for comparing time of execution of the original and the edited versions of experiment scripts
+
+
+
+
---
+++ compare_time.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# --------------------------------------------
+# make a time comparison
+# --------------------------------------------
+
+# specify experiment - number of iterations 
+niter = 50
+
+# use function name check to avoid infinite cycles/incorrect time measurement 
+if __name__ == '__main__':
+    import time
+    
+# run experiment with multiprocessing and measure time
+    start_time_multi = time.perf_counter()
+    exec(open('experiment_script_multiprocessing.py').read())
+    finish_time_multi = time.perf_counter()
+    
+    run_time_multi = finish_time_multi - start_time_multi
+    print(f'Execution time of the script running in parallel is {round(run_time_multi,2)}, amount of iterations is {niter}')
+    
+# run original experiment (without multiprocessing) and measure time
+    start_time_orig = time.perf_counter()
+    #exec(open('experiment_script_original.py').read())
+    finish_time_multi = time.perf_counter()
+    
+    run_time_orig = finish_time_multi - start_time_multi
+    print(f'Execution time of the script running in parallel is {round(run_time_orig,2)}, amount of iterations is {niter}')
+    
+    print(f'\nRunning in parallel takes {round(run_time_orig/run_time_multi, 2)} times less time ')
